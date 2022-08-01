b40f17d7b4abb7ba6850faf4a8f99f49df300bbb
Simon Cole <simonccole1@gmail.com>
2022-08-01 11:34:52 -0400

    changed ternary operator in sort callback and the error went away

--- frontend/components/transporterTable.js
+++ frontend/components/transporterTable.js
@@ -3,23 +3,27 @@ import styles from "../styles/Home.module.css";
 export default function TransporterTable(props){
     const transporterDensity = props.transporterDensity;
     return(
+        <div>
             <table className={styles.resultsTable}>
                 <thead>
-                        <tr>
-                            <th className={styles.resultsTable}>Transporter ID <br></br> &#40;Click for more info&#41;</th>
-                            <th className={styles.resultsTable}>Transporter Quantity</th>
-                        </tr>
-                    </thead>
+                    <tr>
+                        <th className={styles.resultsTable}>Transporter ID <br></br> &#40;Click for more info&#41;</th>
+                        <th className={styles.resultsTable}>Transporter Quantity</th>
+                    </tr>
+                </thead>
                     <tbody>
-                        {transporterDensity.tableData.map((element, index) => (
-                            <tr key={index}>
-                                <td className={styles.resultsTable}>
-                                    <a className={styles.transporterLink} target="_blank" href={`https://www.tcdb.org/search/result.php?tc=${element.transporter}`}>{element.transporter}</a>
-                                </td>
-                                <td className={styles.resultsTable}>{element.count}</td>
-                            </tr>
-                        ))}
+                        {transporterDensity.tableData.map((element, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td className={styles.resultsTable}>
+                                        <a className={styles.transporterLink} target="_blank" href={`https://www.tcdb.org/search/result.php?tc=${element.transporter}`}>{element.transporter}</a>
+                                    </td>
+                                    <td className={styles.resultsTable}>{element.count}</td>
+                                </tr>
+                            )
+                        })}
                     </tbody>
             </table>
+        </div>
     );
 }
