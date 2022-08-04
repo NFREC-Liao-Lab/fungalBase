c84c106e4280607111e7f23d08b7cfb2dca0d55a
Simon Cole <simonccole1@gmail.com>
2022-08-04 13:01:45 -0400

    added landing page for MicroFisher

---
+++ frontend/pages/microFisher/index.js
@@ -0,0 +1,10 @@
+import styles from "../../styles/Home.module.css";
+
+export default function microFisher(){
+    return(
+        <div className={styles.microFisherWrapper}>
+            <h1 className={styles.title}>MicroFisher</h1>
+            <p>This page is currently under development, but in the future will be a user-friendly GUI for the MicroFisher package.</p>
+        </div>
+    )
+}
