6fb5f4ebaae6d39f3338215c73a488fc7c2a5a7b
Simon Cole <simonccole1@gmail.com>
2022-08-05 16:57:28 -0400

    Started work refactoring genus page use reactQuery

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,12 +2,16 @@ import '../styles/globals.css'
 import Layout from '../components/layouts.js'
 import Head from 'next/head'
 import Image from 'next/image'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+const queryClient = new QueryClient();
 
 export default function myApp({ Component, pageProps }) {
   return(
     <Layout>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </Layout>
   );
 }
